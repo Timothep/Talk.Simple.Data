74434ea1ba6f74aa6d04a2b05a72e4284c7e77b4
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-09 12:18:40 +0200

    First demo

---
+++ Simple.Data.First.Demo/Simple.Data.First.Demo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple.Data.First.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=world;Integrated Security=True";
+            dynamic db = Database.OpenConnection(connectionString);
+
+            dynamic cities = db.city.FindAllByCountryCode("USA")
+                                    .Where(db.city.Population > 1000000);
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city.Name);
+            }
+        }
+    }
+}
