9c1e422125c01f89c5269570bcabb4e6208a398d
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-09 17:14:12 +0200

    Added the MongoDb Variant of the code

--- Simple.Data.First.Demo/Simple.Data.First.Demo/Program.cs
+++ Simple.Data.First.Demo/Simple.Data.First.Demo/Program.cs
@@ -10,16 +10,7 @@ namespace Simple.Data.First.Demo
     {
         static void Main(string[] args)
         {
-            const string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=world;Integrated Security=True";
-            dynamic db = Database.OpenConnection(connectionString);
 
-            dynamic cities = db.city.FindAllByCountryCode("USA")
-                                    .Where(db.city.Population > 1000000);
-
-            foreach (var city in cities)
-            {
-                Console.WriteLine(city.Name);
-            }
         }
     }
 }
