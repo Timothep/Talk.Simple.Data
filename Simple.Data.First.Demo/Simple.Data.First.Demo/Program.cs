a1022ef7c9d6dc5ee6677874dffae52db3deeb3a
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-10 13:15:27 +0200

    Added the program logic to the first example

--- Simple.Data.First.Demo/Simple.Data.First.Demo/Program.cs
+++ Simple.Data.First.Demo/Simple.Data.First.Demo/Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Simple.Data.First.Demo
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            const string conStr = @"Data Source=.\SQLEXPRESS;Initial Catalog=world;Integrated Security=True";
+            //const string conStr = @"mongodb://localhost:27017/world";
 
+            var db = Database.OpenConnection(conStr);
+
+            var cities = db.city.FindAllByCountryCode("USA").Where(db.city.Population > 1000000);
+
+            foreach (var city in cities)
+                Console.WriteLine(city.Name);
+
+            Console.ReadLine();
         }
     }
 }
