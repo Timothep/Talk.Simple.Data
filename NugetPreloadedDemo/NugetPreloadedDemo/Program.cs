53ef5787dc6b6952d8ed1749ce460db935964b29
Tim Bourguignon <timothepdotnet@gmail.com>
2013-06-16 11:08:14 +0200

    Cleanup after the Dotnet Day Franken Conference

---
+++ NugetPreloadedDemo/NugetPreloadedDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using Simple.Data;
+
+namespace NugetPreloadedDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string sqlConnString = @"Data Source=.\SQLEXPRESS;Initial Catalog=MvcMusicStore;Integrated Security=True";
+            const string mongoConnString = @"mongodb://localhost:27017/MvcMusicStore";
+        }
+    }
+}
--- NugetPreloadedDemo/NugetPreloadedDemo/Properties/AssemblyInfo.cs
+++ NugetPreloadedDemo/NugetPreloadedDemo/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Simple.Data.First.Demo")]
+[assembly: AssemblyTitle("NugetPreloadedDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Simple.Data.First.Demo")]
+[assembly: AssemblyProduct("NugetPreloadedDemo")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("72017816-5a52-4036-a6ce-12ce83e40e30")]
+[assembly: Guid("6f096e62-8466-4db0-8c87-883d65a56bba")]
 
 // Version information for an assembly consists of the following four values:
 //
